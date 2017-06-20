0470c85ab329ba7d47b1854f1a6bde1c4df8f1aa
kristajg <krista.goralczyk@conciergeauctions.com>
2017-06-20 12:19:45 -0500

    Lightning effect on rain option

--- components/Rain.js
+++ components/Rain.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, Animated, View, Easing } from 'react-native';
 import Raindrop from './Raindrop';
 
 export default class Rain extends React.Component {
@@ -9,10 +9,53 @@ export default class Rain extends React.Component {
 
   state = {
     numRaindrops: 50, // TODO: accept numRaindrops or rainIntensity as props when its a full library
+    // lightningEffectsOn: true, // TODO: accept lightningEffectsOn as a boolean prop
+    lightningValue: new Animated.Value(1),
   };
 
+  componentDidMount() {
+    // if(this.state.lightningEffectsOn) this.startLightning();
+    this.startLightning();
+  }
+
+  startLightning = () => {
+    Animated.sequence([
+      Animated.timing(
+        this.state.lightningValue,
+        {
+          toValue: 0.2,
+          delay: Math.floor(Math.random() * 5000) + 2000,
+          duration: 300,
+          easing: Easing.easeOutElastic,
+        }
+      ),
+      Animated.timing(
+        this.state.lightningValue,
+        {
+          toValue: 1,
+          duration: 150,
+        }
+      ),
+      Animated.timing(
+        this.state.lightningValue,
+        {
+          toValue: 0.2,
+          duration: 150,
+          easing: Easing.easeOutElastic,
+        }
+      ),
+      Animated.timing(
+        this.state.lightningValue,
+        {
+          toValue: 1,
+          duration: 150,
+        }
+      ),
+    ]).start(() => this.startLightning());
+  }
+
   render() {
-    const { numRaindrops } = this.state;
+    const { numRaindrops, lightningValue } = this.state;
     const drops = [];
 
     for(let i=0; i<numRaindrops; i++) {
@@ -22,10 +65,14 @@ export default class Rain extends React.Component {
     }
 
     return (
-      <View style={styles.container}>
-        <Text style={styles.title}>Make it rain ;)</Text>
+      <Animated.View style={[
+        styles.container,
+        {
+          opacity: lightningValue,
+        }
+      ]}>
         {drops}
-      </View>
+      </Animated.View>
     );
   }
 }
@@ -33,7 +80,7 @@ export default class Rain extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#ccc',
+    backgroundColor: '#434343',
     alignItems: 'center',
     justifyContent: 'center',
   },
